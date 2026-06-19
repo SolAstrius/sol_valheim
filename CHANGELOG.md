27b8df1d30b4b3fb09bc11cc6f12d90b656a67a9
Ivan Filipenkov <fox@vanutp.dev>
2026-06-19 19:57:11 +0200

    publishing preparations: metadata fixes, changelog, minotaur plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+### 2.0.0
+
+- 1.21.1 neoforge port
+- set `eatAgainPercentage` to 0.5 by default, like in valheim
+- fix food bar overlapping with other bars (vehicle health, air, thirst, etc.)
+- don't render food overlay in creative/spectator
+- preserve food when passing through the end portal
+- various optimizations
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("net.neoforged.moddev") version "2.0.78"
+    id("com.modrinth.minotaur") version "2.+"
 }
 
-version = "1.1.2"
+version = "2.0.0"
 group = "vice"
 base { archivesName = "sol_valheim" }
 
@@ -50,5 +51,25 @@ tasks.named<ProcessResources>("processResources") {
     inputs.property("version", project.version)
     filesMatching("META-INF/neoforge.mods.toml") {
         expand("version" to project.version)
+    }
+}
+
+modrinth {
+    versionName = project.version.toString()
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("sol-valheim")
+    versionType.set("release")
+    changelog.set(
+        file("$rootDir/CHANGELOG.md")
+            .readText()
+            .split("###")[1]
+            .let { x -> "###$x" }
+    )
+    gameVersions.add("1.21.1")
+    loaders.add("neoforge")
+    uploadFile.set(tasks.jar)
+    dependencies {
+        required.project("cloth-config")
+        optional.project("farmers-delight")
     }
 }
